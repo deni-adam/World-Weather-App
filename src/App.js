ae2d2cdb0803ee34a8deeffbd3b94d60eba47906
Jozef Cipa <cipa.jozef@gmail.com>
2022-10-12 14:58:02 +0200

    fix: load images from public/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import {ReactComponent as RainyIcon} from './weather_icons/animated/rainy-4.svg'
 
 function App() {
   return (
     <div className="App">
     ahoj
-    <RainyIcon/>
+    <img src="/weather_icons/animated/rainy-4.svg" />
     </div>
   );
 }
