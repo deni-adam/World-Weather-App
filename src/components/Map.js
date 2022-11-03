0acb7ead4b35404ccd043cf2d972eba66d804e9b
deni-adam <denisa.adamcova@email.cz>
2022-11-03 23:45:10 +0100

    draggable marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,68 +1,70 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import 'mapbox-gl/dist/mapbox-gl.css'; 
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import "mapbox-gl/dist/mapbox-gl.css";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhZGU2IiwiYSI6ImNsOThmcTdibzA2b2gzd3A4M2MxdnI0NDIifQ.BuN219pi4oXWr46bZ0hkvA';
-
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWRhZGU2IiwiYSI6ImNsOThmcTdibzA2b2gzd3A4M2MxdnI0NDIifQ.BuN219pi4oXWr46bZ0hkvA";
 
 function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lon, setLon] = useState(14);
+  const [lat, setLat] = useState(50);
+  const [zoom, setZoom] = useState(2);
+  const marker = useRef(null);
 
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lon, setLon] = useState(14);
-    const [lat, setLat] = useState(50);
-    const [zoom, setZoom] = useState(2);
-    const marker = useRef(null)
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lon, lat],
+      zoom: zoom,
+    });
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lon, lat],
-            zoom: zoom
-        });
+    // Add zoom and rotation controls to the map.
+    map.current.addControl(new mapboxgl.NavigationControl());
+  }, []);
 
-        // Add zoom and rotation controls to the map.
-        map.current.addControl(new mapboxgl.NavigationControl());
-    }, []);
-
-    const showMarker = () => {
-        // if map is defined and marker is not yet there
-        if (map.current && !marker.current) {
-            // add marker
-            marker.current = new mapboxgl.Marker()
-                .setLngLat([14.4378, 50.0755])
-                .addTo(map.current);
-        }
+  const showMarker = () => {
+    // if map is defined and marker is not yet there
+    if (map.current && !marker.current) {
+      // add marker
+      marker.current = new mapboxgl.Marker({
+        draggable: true,
+      })
+        .setLngLat([14.4378, 50.0755])
+        .addTo(map.current);
     }
+  };
 
-    const removeMarker = () => {
-        if (map.current) {
-            // if marker already exists, remove it first
-            if (marker.current) {
-                marker.current.remove()
-                marker.current = null
-            }
-        }
+  console.log(lon, lat);
+
+  const removeMarker = () => {
+    if (map.current) {
+      // if marker already exists, remove it first
+      if (marker.current) {
+        marker.current.remove();
+        marker.current = null;
+      }
     }
-    
+  };
 
-    return (
-        <>
-        <button onClick={showMarker}>zobrazit bod</button>  
-        <button onClick={removeMarker}>skryt bod</button>  
+  return (
+    <>
+      <button onClick={showMarker}>zobrazit bod</button>
+      <button onClick={removeMarker}>skryt bod</button>
 
-            <div ref={mapContainer}
-                className="map-container"
-                style={{
-                    width: '90%',
-                    margin: '20px',
-                }}
-            />
-        </>
-    )
-
+      <div
+        ref={mapContainer}
+        className="map-container"
+        style={{
+          width: "90%",
+          margin: "20px",
+        }}
+      />
+    </>
+  );
 }
 
 export default Map;
--- src/components/WeatherInfoCard.js
+++ src/components/WeatherInfoCard.js
@@ -1,54 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CardHeader from '@mui/material/CardHeader';
+import React, { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CardHeader from "@mui/material/CardHeader";
 
 const weatherIcons = {
-    '02n': '/weather_icons/animated/cloudy-day-1.svg',
-    '50d': '/weather_icons/animated/cloudy.svg',
-    '01d': '/weather_icons/animated/day.svg',
-    '01n': '/weather_icons/animated/night.svg'
-}
+  "01d": "/weather_icons/animated/day.svg",
+  "01n": "/weather_icons/animated/night.svg",
+  "02d": "/weather_icons/animated/cloudy-day-2.svg",
+  "02n": "/weather_icons/animated/cloudy-night-3.svg",
+  "03d": "/weather_icons/animated/cloudy.svg",
+  "03n": "/weather_icons/animated/cloudy.svg",
+  "04d": "/weather_icons/animated/cloudy.svg",
+  "04n": "/weather_icons/animated/cloudy.svg",
+  "50d": "/weather_icons/animated/cloudy.svg",
+  "50n": "/weather_icons/animated/cloudy.svg",
+  "09d": "/weather_icons/animated/rainy-7.svg",
+  "09n": "/weather_icons/animated/rainy-7.svg",
+  "10d": "/weather_icons/animated/rainy-5.svg",
+  "10n": "/weather_icons/animated/rainy-5.svg",
+  "11d": "/weather_icons/animated/thunder.svg",
+  "11n": "/weather_icons/animated/thunder.svg",
+  "13d": "/weather_icons/animated/snowy-6.svg",
+  "13n": "/weather_icons/animated/snowy-6.svg",
+};
 
-function WeatherInfoCard({city, weather}) {
-    return (
-        <>
-            <Card
-                style={{
-                    marginLeft: '20px',
-                    marginRight: '20px',
-                    marginTop: '20px',
-                }}>
-                <CardContent
-                // currentWeather={currentWeather}
-                >
-                    <CardHeader
-                        title={city?.name}
-                        //   {{city?.name} {city?.country}}
-                        subheader="Today"
-                    />
-                    <CardMedia
-                        component="img" 
-                        style={{
-                            width: '60%',
-                            display: 'block',
-                            marginLeft: 'auto',
-                            marginRight: 'auto',
-                        }}
-                        image={weatherIcons[weather?.weather[0]?.icon]}
-                        alt="Weather animation"
-                    />
+function WeatherInfoCard({ city, weather }) {
+  return (
+    <>
+      <Card
+        style={{
+          marginLeft: "20px",
+          marginRight: "20px",
+          marginTop: "20px",
+        }}
+      >
+        <CardContent
+        // currentWeather={currentWeather}
+        >
+          <CardHeader
+            title={city?.name}
+            //   {{city?.name} {city?.country}}
+            subheader="Today"
+          />
+          <CardMedia
+            component="img"
+            style={{
+              width: "60%",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+            image={weatherIcons[weather?.weather[0]?.icon]}
+            alt="Weather animation"
+          />
+        </CardContent>
 
-                </CardContent>
-
-                <div>{weather?.weather?.[0]?.description}</div>
-                <div>Temperature: {weather?.main?.temp} °C</div>
-                <div>Feels like: {weather?.main?.feels_like} °C</div>
-
-            </Card>
-        </>
-    )
+        <div>{weather?.weather?.[0]?.description}</div>
+        <div>Temperature: {weather?.main?.temp} °C</div>
+        <div>Feels like: {weather?.main?.feels_like} °C</div>
+      </Card>
+    </>
+  );
 }
 
 export default WeatherInfoCard;
