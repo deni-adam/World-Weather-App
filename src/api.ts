893ce5bcb8408b1df6b051a1eb5886787699084f
Jozef Cipa <cipa.jozef@gmail.com>
2022-11-14 23:07:44 +0100

    feat: add typescript

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,13 @@
 
 const apiKey = process.env.REACT_APP_API_KEY
 
-
-export const fetchWeather = async (lat, lon) => {
+export const fetchWeather = async (lat: string, lon: string) => {
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
 
     return await res.json()
 }
 
-export const geocodeCity = async (city) =>{ 
+export const geocodeCity = async (city: string) =>{ 
     const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
 
     return await res.json()
